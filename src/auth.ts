b1156e728c8c6f41b60e2eded85e0043f856f37d
inoueshoei <inoueshoei.313.9720@gmail.com>
2024-07-31 23:21:08 +0900

    [Add]カスタムログインページ作成

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,45 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import Discord from "next-auth/providers/discord"
+import type { Provider } from "next-auth/providers"
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-	providers: [
-		GitHub,
-	],
+const providers: Provider[] = [
+	Google,
+	GitHub,
+	Discord,
+]
+
+export const providerMap = providers.map((provider) => {
+	if (typeof provider === "function") {
+		const providerData = provider()
+		return { id: providerData.id, name: providerData.name }
+	} else {
+		return { id: provider.id, name: provider.name }
+	}
 })
 
-declare module "next-auth" {
-	interface Session {
-		accessToken?: string
+export const { handlers, auth, signIn, signOut } = NextAuth({
+	providers,
+	pages: {
+		signIn: "/signin",
+	},
+	basePath: "/api/auth",
+	callbacks: {
+		authorized: async ({request, auth}) => {
+			try {
+				const { pathname } = request.nextUrl;
+				// Authentication required except for the root and sign-in pages.
+				if (pathname !== "/" && pathname !== "/api/auth/signin") return !!auth;
+				return true;
+			} catch (e) {
+				console.log(e);
+			}
+		},
+		jwt: async ({ token, trigger, session }) => {
+			if (trigger === "update") token.name = session.user.name;
+			return token;
+		}
 	}
-}
+})
+
